5bdd07f4de43cb744439340ca2761b3688471897
Jamie Bland <jamie@theblands.net>
2022-03-23 23:16:07 -0500

    Created and uploaded SimpleList project

---
+++ SimpleList/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Jamie Bland (jamie@theblands.net)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+#include "node.h"
+
+int main()
+{
+
+
+    return 0;
+}
